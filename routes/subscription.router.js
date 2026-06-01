c936e12cb3a8f1ab838b437e96a1191a180c9a36
Ansharora124 <ansharora13b@gmail.com>
2026-06-01 23:11:39 +0530

    subscription model

--- routes/subscription.router.js
+++ routes/subscription.router.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-
+import { authorize } from "../middleware/authorize.js";
+import {createSubscription} from "../controller/subscription.controler.js";
+import {getUserSubscriptions} from "../controller/subscription.controler.js";
 const subsRouter=Router();
 
 
@@ -7,13 +9,13 @@ subsRouter.get('/',(req,res)=>res.send({title:'get all subscription'}));
 
 subsRouter.get('/:id',(req,res)=>res.send({title:'get subs detail'}));
 
-subsRouter.post('/',(req,res)=>res.send({title:'create subscription'}));
+subsRouter.post('/',authorize,createSubscription);
 
 subsRouter.put('/:id',(req,res)=>res.send({title:'update subscription'}));
 
 subsRouter.delete('/:id',(req,res)=>res.send({title:'delete subscription'}));
 
-subsRouter.get('/user/:id',(req,res)=>res.send({title:'get all user subscription '}));
+subsRouter.get('/user/:id',authorize,getUserSubscriptions);
 
 subsRouter.put('/:id/cancel',(req,res)=>res.send({title:'cancel subscription'}));
 
